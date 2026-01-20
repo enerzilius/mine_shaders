57f8dba32ab14667e4cef857697e75a60a753ccb
enerzilius <efblouback@gmail.com>
2026-01-19 21:27:09 -0300

    feat: iluminação com lightmap

--- shaders/basic_frag.glsl
+++ shaders/basic_frag.glsl
@@ -2,16 +2,19 @@
 
 in vec2 texCoord;
 in vec4 foliageColor;
+in vec2 lightMapCoordinates;
 
 uniform sampler2D gtexture;
+uniform sampler2D lightmap;
 
 /* DRAWBUFFERS:0 */
 layout(location = 0) out vec4 outColor0;
 
 void main() {
+  vec4 lightColor = texture(lightmap, lightMapCoordinates);
   vec4 outputColor = texture(gtexture, texCoord);
   if(outputColor.a < .1) discard;
-  if(foliageColor.a > .1) outputColor *= foliageColor;
-  
+  if(foliageColor.a > .1) outputColor *= foliageColor * lightColor;
+  //if(foliageColor.a > .1) outputColor *= foliageColor;
   outColor0 = outputColor;
 } 
---
+++ shaders/basic_vert.glsl
@@ -0,0 +1,26 @@
+#version 460
+
+in vec3 vaPosition;
+in vec2 vaUV0;
+in vec4 vaColor;
+in uvec2 vaUV2;
+
+out vec2 texCoord;
+out vec4 foliageColor;
+out vec2 lightMapCoordinates;
+
+uniform vec3 chunkOffset;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+uniform mat4 gbufferModelViewInverse;
+uniform vec3 cameraPosition;
+
+void main() {
+  texCoord = vaUV0;
+  foliageColor = vaColor;
+  lightMapCoordinates = vaUV2 * (1.0/256.0) + (1.0/32.0);
+
+  vec3 worldPosition = cameraPosition + (gbufferModelViewInverse * modelViewMatrix * vec4(vaPosition+chunkOffset, 1)).xyz;
+  float distance = distance(worldPosition, cameraPosition);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(vaPosition+chunkOffset,1);
+}
